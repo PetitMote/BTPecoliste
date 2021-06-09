6334080507925bfbfa6df357398d45900954cea7
Moté <automates@lepouete.fr>
2021-06-09 22:39:34 +0200

    Settings for the use of PostgreSQL and PostGIS.
    Changed the Django settings so it uses PostgreSQL as the database engine, with the PostGIS extension, through django.contrib.gis.

--- BTPecoliste/settings.py
+++ BTPecoliste/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'ecoliste'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,9 @@ WSGI_APPLICATION = 'BTPecoliste.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'BTPecoliste',
+        'USER': 'timothee'
     }
 }
 

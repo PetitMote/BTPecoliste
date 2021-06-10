ad29498e6c4d9019916d8954027ad798f8282fb0
Moté <automates@lepouete.fr>
2021-06-10 21:25:29 +0200

    Added Contact model and str methods.

--- ecoliste/models.py
+++ ecoliste/models.py
@@ -19,6 +19,9 @@ class Enterprise(models.Model):
     added = models.DateField("Date d'ajout", auto_now_add=True)
     updated = models.DateField("Date de mise à jour", auto_now=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Address(models.Model):
     """
@@ -32,6 +35,9 @@ class Address(models.Model):
     geolocation = models.PointField("Coordonnées", geography=True, null=False)
     is_production = models.BooleanField("Est un lieu de production")
 
+    def __str__(self):
+        return self.text_version
+
 
 class MaterialTypeCategory(models.Model):
     """
@@ -39,6 +45,9 @@ class MaterialTypeCategory(models.Model):
     """
     name = models.CharField("Nom de la catégorie", max_length=200, null=False)
     order = models.PositiveSmallIntegerField("Ordre d'affichage", null=False, default=99)
+
+    def __str__(self):
+        return self.name
 
 
 class MaterialType(models.Model):
@@ -49,6 +58,9 @@ class MaterialType(models.Model):
                                  verbose_name="Catégorie d'usage", related_name="usages")
     name = models.CharField("Usage de matériaux", max_length=200, null=False)
     order = models.PositiveSmallIntegerField("Ordre d'affichage", null=False, default=99)
+
+    def __str__(self):
+        return self.name
 
 
 class MaterialByEnterprise(models.Model):
@@ -62,6 +74,9 @@ class MaterialByEnterprise(models.Model):
     type = models.ForeignKey(MaterialType, on_delete=models.CASCADE, verbose_name="Usage", related_name="products")
     origin = models.CharField("Origine", max_length=50, null=False)
 
+    def __str__(self):
+        return f"{self.type} produit par {self.enterprise}"
+
 
 class MaterialProductionAddress(models.Model):
     """
@@ -72,12 +87,18 @@ class MaterialProductionAddress(models.Model):
     address = models.ForeignKey(Address, on_delete=models.CASCADE, verbose_name="Addresse de production",
                                 related_name="materials")
 
+    def __str__(self):
+        return f"{self.material} à {self.address}"
+
 
 class BiobasedOriginMaterial(models.Model):
     """
     Based materials / origins for the biobased materials (wood, straw...).
     """
     name = models.CharField("Nom", max_length=50, null=False)
+
+    def __str__(self):
+        return self.name
 
 
 class LinkBiobasedMaterial(models.Model):
@@ -88,3 +109,19 @@ class LinkBiobasedMaterial(models.Model):
                                  verbose_name="Matériau de l'entreprise", related_name="biobased_origins")
     biobased_origin = models.ForeignKey(BiobasedOriginMaterial, on_delete=models.CASCADE,
                                         verbose_name="Origine biosourcée", related_name="based_materials")
+
+    def __str__(self):
+        return f"{self.material} en {self.biobased_origin}"
+
+
+class Contact(models.Model):
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE, verbose_name="Entreprise", related_name="contacts")
+    firstname = models.CharField("Prénom", max_length=50, null=False)
+    surname = models.CharField("Nom de famille", max_length=50, null=False)
+    description = models.TextField("Description", max_length=200)
+    phone1 = models.CharField("Téléphone 1", max_length=25)
+    phone2 = models.CharField("Téléphone 2", max_length=25)
+    mail = models.EmailField("Adresse mail")
+
+    def __str__(self):
+        return f"{self.firstname} {self.surname}"

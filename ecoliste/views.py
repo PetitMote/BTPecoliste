853ecc984f72dfce4fbca394b2cef8f921ff4225
Moté <automates@lepouete.fr>
2021-06-12 21:33:13 +0200

    First view (WIP)

--- ecoliste/views.py
+++ ecoliste/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.http import Http404
 
 # Create your views here.
+from .models import Enterprise, Address
+
+
+def search_page(request):
+    return HttpResponse("Page de recherche de matériaux")
+
+
+def enterprise(request, enterprise_id):
+    enterprise = get_object_or_404(Enterprise, pk=enterprise_id)
+    addresses = Address.objects.filter(enterprise=enterprise)
+    context = {
+        "page_title": enterprise.name,
+        "enterprise": enterprise,
+        "addresses": addresses,
+    }
+    return render(request, "ecoliste/enterprise.html", context)

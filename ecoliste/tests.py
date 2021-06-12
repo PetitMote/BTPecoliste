8c671c18a893565f21adcd79712ed1193d765cfc
Moté <automates@lepouete.fr>
2021-06-12 21:32:52 +0200

    Tests for the material_origin validator

--- ecoliste/tests.py
+++ ecoliste/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+import random
 
-# Create your tests here.
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from .models import validate_material_origin
+from BTPecoliste.settings import BTP_ECOLISTE_SETTINGS
+
+
+class ValidateMaterialOriginTests(TestCase):
+    def test_raises_no_error_for_correct_entry(self):
+        correct_answers = BTP_ECOLISTE_SETTINGS["material_origins"]
+        value = correct_answers[random.randint(0, len(correct_answers) - 1)]
+        validate_material_origin(value)
+
+    def test_raises_error_for_invalid_entry(self):
+        value = "eknkenglkengz"
+        self.assertRaises(
+            ValidationError,
+            validate_material_origin,
+            [
+                value,
+            ],
+        )
+
+    def test_raises_error_for_invalid_case(self):
+        correct_answers = BTP_ECOLISTE_SETTINGS["material_origins"]
+        value = str(
+            correct_answers[random.randint(0, len(correct_answers) - 1)]
+        ).upper()
+        self.assertRaises(
+            ValidationError,
+            validate_material_origin,
+            [
+                value,
+            ],
+        )

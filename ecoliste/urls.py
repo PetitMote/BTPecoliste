981ed8fadf703496153916000a691e43bf9e1c2f
Moté <automates@lepouete.fr>
2021-06-13 19:44:15 +0200

    Translation ready

--- ecoliste/urls.py
+++ ecoliste/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.utils.translation import gettext_lazy as _
 
 from . import views
 
+app_name = "ecoliste"
 urlpatterns = [
-    path("", views.search_page, name="search_page"),
-    path("structure/<int:enterprise_id>/", views.enterprise, name="enterprise"),
+    path(_(""), views.search_page, name="search"),
+    path(_("entreprise/<int:enterprise_id>/"), views.enterprise, name="enterprise"),
 ]

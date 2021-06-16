e82d356d9d2fada3df5d384c1c4f817fbd0e662b
Moté <automates@lepouete.fr>
2021-06-16 16:18:11 +0200

    Regroup tag usage confirmed

--- ecoliste/views.py
+++ ecoliste/views.py
@@ -17,20 +17,6 @@ def enterprise_view(request, enterprise_id):
         .select_related("type", "type__category", "origin")
         .prefetch_related("address")
     )
-    # types = {}
-    # for material in materials:
-    #     if material.type in types.keys():
-    #         types[material.type].append(material)
-    #     else:
-    #         types[material.type] = [material]
-    # materials_categorised = {}
-    # for type in types.keys():
-    #     if type.category in materials_categorised.keys():
-    #         materials_categorised[type.category][type] = types[type]
-    #     else:
-    #         materials_categorised[type.category] = {type: types[type]}
-
-    # materials = enterprise.products.all().values("type__category", "type", "origin")
 
     context = {
         "enterprise": enterprise,

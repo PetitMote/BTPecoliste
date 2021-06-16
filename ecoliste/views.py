90e38c269a1cfcbd36576e061e9ea8249433585b
Moté <automates@lepouete.fr>
2021-06-16 16:11:22 +0200

    First draft of the regroup tag usage

--- ecoliste/views.py
+++ ecoliste/views.py
@@ -17,23 +17,25 @@ def enterprise_view(request, enterprise_id):
         .select_related("type", "type__category", "origin")
         .prefetch_related("address")
     )
-    types = {}
-    for material in materials:
-        if material.type in types.keys():
-            types[material.type].append(material)
-        else:
-            types[material.type] = [material]
-    materials_categorised = {}
-    for type in types.keys():
-        if type.category in materials_categorised.keys():
-            materials_categorised[type.category][type] = types[type]
-        else:
-            materials_categorised[type.category] = {type: types[type]}
+    # types = {}
+    # for material in materials:
+    #     if material.type in types.keys():
+    #         types[material.type].append(material)
+    #     else:
+    #         types[material.type] = [material]
+    # materials_categorised = {}
+    # for type in types.keys():
+    #     if type.category in materials_categorised.keys():
+    #         materials_categorised[type.category][type] = types[type]
+    #     else:
+    #         materials_categorised[type.category] = {type: types[type]}
+
+    # materials = enterprise.products.all().values("type__category", "type", "origin")
 
     context = {
         "enterprise": enterprise,
         "addresses": addresses,
-        "materials": materials_categorised,
+        "materials": materials,
         "contacts": contacts,
     }
     return render(request, "ecoliste/enterprise.html", context)
